2bb06b3fa9cc3f6da83855b672579fd15e59f5a1
Akira Otaka <hydrangeas.project@gmail.com>
2023-10-25 12:01:19 +0000

    iroiro

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    mySecret: "secret",
+    secondSecret: process.env.AZURE_STORAGE_CONNECTION_STRING, // Pass through env variables
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
